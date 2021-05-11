fad0316783043cb526348f631870f91fcf2b0c8d
Ricardo Carrasco Soto <rcarrascosoto@bancoripley.com>
2021-05-11 14:00:15 -0400

    Fix importaciones

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -2,16 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 //Angular Material Components
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import { MatIconModule } from '@angular/material/icon';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field'
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-
-import { MatButtonModule, MatCardModule, MatCheckboxModule, MatDialogModule, MatFormFieldModule, MatIconModule, MatInputModule } from '@angular/material';
+import { MatButtonModule, MatCardModule, MatCheckboxModule, MatDialogModule, MatFormFieldModule, MatIconModule, MatInputModule, MatSelectModule } from '@angular/material';
 
 const materialComponents = [
   MatButtonModule,
@@ -20,7 +11,8 @@ const materialComponents = [
   MatDialogModule,
   MatFormFieldModule,
   MatInputModule,
-  MatSelectModule
+  MatSelectModule,
+  MatIconModule
 ]
 
 @NgModule({
