15d5292df2225d5106bfede9028a5ea783ade589
David Moya <dav.moyam@outlook.cl>
2021-07-12 20:46:22 -0400

    [updated] the name of the component search result to search results

--- src/app/pages/search-restaurant/search-restaurant.module.ts
+++ src/app/pages/search-restaurant/search-restaurant.module.ts
@@ -4,12 +4,12 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { SearchRestaurantComponent } from './search-restaurant.component';
 import { MaterialModule } from 'src/app/shared/material/material.module';
 import { SearchRestaurantRoutingModule } from './search-restautant-routing.module';
-import { SearchResultComponent } from './search-result/search-result.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
 
 @NgModule({
   declarations: [
     SearchRestaurantComponent,
-    SearchResultComponent
+    SearchResultsComponent
   ],
   imports: [
       CommonModule,
--- src/app/pages/search-restaurant/search-results/search-results.component.ts
+++ src/app/pages/search-restaurant/search-results/search-results.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-search-result',
-  templateUrl: './search-result.component.html',
-  styleUrls: ['./search-result.component.scss']
+  selector: 'app-search-results',
+  templateUrl: './search-results.component.html',
+  styleUrls: ['./search-results.component.scss']
 })
-export class SearchResultComponent implements OnInit {
+export class SearchResultsComponent implements OnInit {
 
   public restaurants = [{
     isFavorite: true
