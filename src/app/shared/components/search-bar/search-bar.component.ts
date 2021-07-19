a7a4ebbaf649c481b4a7c26ff7bc7c381813b520
David Moya <dav.moyam@outlook.cl>
2021-07-19 17:26:47 -0400

    [updated] search bar component, added new behaviors to date element

--- src/app/shared/components/search-bar/search-bar.component.ts
+++ src/app/shared/components/search-bar/search-bar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-bar',
@@ -10,27 +10,21 @@ import { Router } from '@angular/router';
 export class SearchBarComponent implements OnInit {
 
   public form!: FormGroup;
+  public minDate = new Date();
 
   constructor(
     private fb: FormBuilder,
-    private route: Router
+    private route: Router,
+    private router: ActivatedRoute
   ) { }
 
   ngOnInit() {
     this.initForm();
-  }
-
-  private initForm() {
-    this.form = this.fb.group({
-      name: ['', [Validators.minLength(3), Validators.required]],
-      dateReservation: ['', [Validators.required]],
-      state: ['', [Validators.required]],
-    });
+    this.getQueryParams();
   }
 
   public search() {
-
-    if (this.form.invalid) return;
+    if (!this.isReadyToSearch()) return;
 
     this.route.navigate(["buscar-locales"], {
       queryParams: {
@@ -41,16 +35,67 @@ export class SearchBarComponent implements OnInit {
     });
   }
 
+  public isReadyToSearch(): boolean {
+
+    if (this.form.get("name").invalid && this.form.get("dateReservation").invalid && this.form.get("state").invalid) {
+      return false;
+    } else if (this.form.get("name").hasError("minlength") || this.form.get("dateReservation").hasError("invalidDate")) {
+      return false;
+    }
+
+    this.resetErrors();
+
+    return true;
+  }
+
   public nameErrorMessage() {
     return this.form.get("name").hasError("minlength") ? "Debe tener mínimo 3 caracteres" :
       this.form.get("name").hasError("required") ? "Debe ingresar un nombre" : null;
   }
 
   public dateReservationErrorMessage() {
-    return this.form.get("dateReservation").hasError("required") ? "Debe ingresar una fecha" : null;
+    return this.form.get("dateReservation").hasError("required") ? "Debe ingresar una fecha válida" :
+      this.form.get("dateReservation").hasError("invalidDate") ? "Debe ser igual o mayor a hoy" : null;
   }
 
   public stateErrorMessage() {
     return this.form.get("state").hasError("required") ? "Debe seleccionar una comuna" : null;
+  }
+
+  private initForm() {
+    this.form = this.fb.group({
+      name: ['', [Validators.minLength(3), Validators.required]],
+      dateReservation: ['', [Validators.required, this.dateValidator]],
+      state: ['', [Validators.required]],
+    });
+  }
+
+  private getQueryParams() {
+    this.router.queryParams.subscribe(x => {
+      this.form.get("name").setValue(x.name);
+      this.form.get("dateReservation").setValue(x.dateReservation ? new Date(x.dateReservation) : null);
+      this.form.get("state").setValue(x.state);
+    })
+  }
+
+  private dateValidator(control: FormControl): { [s: string]: boolean } {
+    if (control.value) {
+
+      var date = new Date(control.value).setHours(0, 0, 0, 0,);
+      var today = new Date().setHours(0, 0, 0, 0,);
+
+      if (date < today) {
+        return { 'invalidDate': true }
+      }
+    }
+
+    console.log("null");
+    return null;
+  }
+
+  private resetErrors() {
+    this.form.get("name").setErrors(null);
+    this.form.get("dateReservation").setErrors(null);
+    this.form.get("state").setErrors(null);
   }
 }
