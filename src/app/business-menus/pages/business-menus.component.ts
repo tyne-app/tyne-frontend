068eabcbdfa706f42fe5ca736d7cba3c7a77ab66
David Moya <dav.moyam@outlook.cl>
2021-08-06 17:12:24 -0400

    [update] cleared the .ts code of business menu component

--- src/app/business-menus/pages/business-menus.component.ts
+++ src/app/business-menus/pages/business-menus.component.ts
@@ -1,6 +1,6 @@
-import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatExpansionPanel } from '@angular/material';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-business-menus',
@@ -9,165 +9,178 @@ import { MatExpansionPanel } from '@angular/material';
 })
 export class BusinessMenusComponent implements OnInit {
 
-  preventSingleClick = false;
-  timer: any;
-  delay: Number;
-  title = "";
+  public form: FormGroup;
+  public panelOpenState = true;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
-  ngOnInit() {
+  get sections() {
+    return this.form.controls["sections"] as FormArray;
   }
 
-  panelOpenState = true;
+  ngOnInit() {
+    this.initForm();
+  }
 
-  accordions = [
-    {
-      id: 1,
-      title: '1',
-      isTitleVisible: false,
-      products: [{
+  public dropSection(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.sections.controls, event.previousIndex, event.currentIndex);
+
+    for (let index = 0; index < this.sections.controls.length; index++) {
+      this.sections.controls[index].get("id").setValue(index + 1);
+    }
+  }
+
+  public addSection() {
+    this.sections.push(this.fb.group({
+      title: ["Nueva Sección", Validators.required],
+      id: ['0'],
+      products: this.addDefaultProduct()
+    }));
+
+    for (let index = 0; index < this.sections.controls.length; index++) {
+      this.sections.controls[index].get("id").setValue(index + 1);
+    }
+  }
+
+  public deleteSection(id: number) {
+    this.sections.removeAt(id);
+
+    for (let index = 0; index < this.sections.controls.length; index++) {
+      this.sections.controls[index].get("id").setValue(index + 1);
+    }
+  }
+
+  public addProduct(seccionId: number) {
+
+    let products = this.sections.controls[seccionId].get("products") as FormArray;
+    products = products ? products : new FormArray([]);
+
+    products.push(this.fb.group({
+      id: ['0'],
+      name: ['Nombre del Producto', Validators.required],
+      description: ['Ingresa una descripción'],
+      imageUrl: ['../../../../../assets/alternate-photo.png'],
+      price: ['0']
+    }));
+  }
+
+  public deleteProduct(seccionId: number, productId: number) {
+    const section = this.sections.controls[seccionId];
+
+    if (section) {
+      const products = section.get("products") as FormArray;
+
+      products.removeAt(productId);
+    }
+  }
+
+  public products(form: any) {
+    return form.controls.products ? form.controls.products.controls : new FormArray([]);
+  }
+
+  private getDataMock() {
+    return [
+      {
         id: 1,
-        name: 'Carne al jugo',
-        imageUrl: 'https://sevilla.abc.es/gurme/wp-content/uploads/sites/24/2012/01/comida-rapida-casera.jpg',
-        price: '4500',
-        description: 'Sabrosa carne al jugo Sabrosa carne al jugo Sabrosa carne al jugo Sabrosa carne al jugo Sabrosa carne al jugo'
+        title: 'Entrada',
+        isTitleVisible: false,
+        products: [{
+          id: 1,
+          name: 'Carne al jugo',
+          imageUrl: 'https://sevilla.abc.es/gurme/wp-content/uploads/sites/24/2012/01/comida-rapida-casera.jpg',
+          price: '4500',
+          description: 'Sabrosa carne al jugo Sabrosa carne al jugo Sabrosa carne al jugo Sabrosa carne al jugo Sabrosa carne al jugo'
+        },
+        {
+          id: 2,
+          name: 'item 2',
+          description: 'Sabrosa carne al jugo Sabrosa carne al jugo Sabrosa carne al jugo Sabrosa carne al jugo Sabrosa carne al jugo',
+          imageUrl: 'https://sevilla.abc.es/gurme/wp-content/uploads/sites/24/2012/01/comida-rapida-casera.jpg',
+          price: '4500',
+        }]
       },
       {
         id: 2,
-        name: 'item 2',
-        description: '',
-        imageUrl: '',
-        price: '4500',
-      }]
-    },
-    {
-      id: 2,
-      title: '2',
-      isTitleVisible: false,
-      products: [{
-        id: 1,
-        name: 'item 1',
-        description: 'description',
-        imageUrl: '',
-        price: '4500',
-      }]
-    },
-    {
-      id: 3,
-      title: '3',
-      isTitleVisible: false,
-      products: [{
-        id: 1,
-        name: 'item 1',
-        description: 'description',
-        imageUrl: '',
-        price: '4500',
-      }]
-    },
-    {
-      id: 4,
-      title: '4',
-      isTitleVisible: false,
-      products: [{
-        id: 1,
-        name: 'item 1',
-        description: 'description',
-        imageUrl: '',
-        price: '4500',
-      }]
-    },
-  ]
-
-  drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.accordions, event.previousIndex, event.currentIndex);
-
-    for (let index = 0; index < this.accordions.length; index++) {
-      this.accordions[index].id = index + 1;
-    }
+        title: 'Almuerzo',
+        isTitleVisible: false,
+        products: [{
+          id: 1,
+          name: 'item 1',
+          description: 'Sabrosa carne al jugo Sabrosa carne al jugo Sabrosa carne al jugo Sabrosa carne al jugo Sabrosa carne al jugo',
+          imageUrl: 'https://sevilla.abc.es/gurme/wp-content/uploads/sites/24/2012/01/comida-rapida-casera.jpg',
+          price: '4500',
+        }]
+      },
+      {
+        id: 3,
+        title: 'Postres',
+        isTitleVisible: false,
+        products: [{
+          id: 1,
+          name: 'item 1',
+          description: 'Sabrosa carne al jugo Sabrosa carne al jugo Sabrosa carne al jugo Sabrosa carne al jugo Sabrosa carne al jugo',
+          imageUrl: 'https://sevilla.abc.es/gurme/wp-content/uploads/sites/24/2012/01/comida-rapida-casera.jpg',
+          price: '4500',
+        }]
+      },
+      {
+        id: 4,
+        title: 'Bebidas',
+        isTitleVisible: false,
+        products: [{
+          id: 1,
+          name: 'item 1',
+          description: 'Sabrosa carne al jugo Sabrosa carne al jugo Sabrosa carne al jugo Sabrosa carne al jugo Sabrosa carne al jugo',
+          imageUrl: 'https://sevilla.abc.es/gurme/wp-content/uploads/sites/24/2012/01/comida-rapida-casera.jpg',
+          price: '4500',
+        }]
+      },
+    ]
   }
 
-  eliminar(id) {
-    this.accordions = this.accordions.filter(x => x.id != id);
-
-    for (let index = 0; index < this.accordions.length; index++) {
-      this.accordions[index].id = index + 1;
-    }
-  }
-
-  addSection() {
-
-    this.accordions.push({
-      id: 0,
-      title: 'Nueva Sección',
-      isTitleVisible: false,
-      products: [{
-        id: 1,
-        name: 'item 1',
-        description: 'Ingresa una descripción',
-        imageUrl: '../../../../../assets/alternate-photo.png',
-        price: '0'
-      }]
+  private initForm() {
+    this.form = this.fb.group({
+      sections: this.fb.array([])
     });
 
-    for (let index = 0; index < this.accordions.length; index++) {
-      this.accordions[index].id = index + 1;
-    }
-  }
-
-  algo(id: number) {
-    this.accordions.find(x => x.id === id).isTitleVisible = true;
-  }
-
-  singleClick(event) {
-    this.preventSingleClick = false;
-    const delay = 200;
-    this.timer = setTimeout(() => {
-      if (!this.preventSingleClick) {
-      }
-    }, delay);
-  }
-
-  blurs(id: number) {
-    this.accordions.find(x => x.id === id).isTitleVisible = false;
-  }
-
-  save(id) {
-    this.accordions.find(x => x.id === id).title = this.title;
-    this.accordions.find(x => x.id === id).isTitleVisible = false;
-    this.title = '';
-  }
-
-  handleSpacebar(ev) {
-    if (ev.keyCode === 32) {
-      ev.stopPropagation();
-    }
-  }
-
-  deleteProduct(seccionId: number, productId: number) {
-    const section = this.accordions.find(x => x.id === seccionId)
-
-    if (section) {
-      section.products = section.products.filter(x => x.id != productId);
-
-      if (section.products.length === 0) {
-        this.accordions = this.accordions.filter(x => x.id != seccionId);
-      }
-    }
-  }
-
-  addProduct(seccionId: number) {
-    const section = this.accordions.find(x => x.id === seccionId)
-    section.products.push({
-      id: 1,
-      name: 'item 1',
-      description: 'Ingresa una descripción',
-      imageUrl: '../../../../../assets/alternate-photo.png',
-      price: '0'
+    this.getDataMock().forEach(x => {
+      this.sections.push(this.fb.group({
+        id: [x.id],
+        title: [x.title, Validators.required],
+        products: this.initProducts(x.products)
+      }));
     });
+  }
 
-    for (let index = 0; index < section.products.length; index++) {
-      section.products[index].id = index + 1;
-    }
+  private initProducts(products: any): FormArray {
+    const formArray = new FormArray([]);
+
+    products.forEach(x => {
+      formArray.push(this.fb.group({
+        id: [x.id],
+        name: [x.name, Validators.required],
+        imageUrl: [x.imageUrl],
+        price: [x.price],
+        description: [x.description]
+      }))
+    })
+
+    return formArray;
+  }
+
+  private addDefaultProduct() {
+    const formArray = new FormArray([]);
+
+    formArray.push(this.fb.group({
+      id: ['0'],
+      name: ['Nombre del Producto', Validators.required],
+      description: ['Ingresa una descripción'],
+      imageUrl: ['../../../../../assets/alternate-photo.png'],
+      price: ['0']
+    }));
+
+    return formArray;
   }
 }
