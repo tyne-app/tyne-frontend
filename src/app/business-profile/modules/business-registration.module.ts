2691596bc1c2e336efe1aa51b2f2baa74a53833a
David Moya <dav.moyam@outlook.cl>
2021-08-09 14:01:50 -0400

    [add] base component + styles, validations and others

--- src/app/business-profile/modules/business-registration.module.ts
+++ src/app/business-profile/modules/business-registration.module.ts
@@ -7,11 +7,20 @@ import { BusinessProfileComponent } from '../pages/business-profile.component';
 import { BusinessProfileRoutingModule } from '../routes/business-registration-routing.module';
 import { BusinessProfileBodyComponent } from '../components/business-profile-body/business-profile-body.component';
 import { SwiperModule } from 'swiper/angular';
+import { BusinessProfileEditPasswordComponent } from '../components/business-profile-edit-password/business-profile-edit-password.component';
+import { BusinessProfileEditDataComponent } from '../components/business-profile-edit-data/business-profile-edit-data.component';
+import { BusinessProfileEditBankDataComponent } from '../components/business-profile-edit-bank-data/business-profile-edit-bank-data.component';
+import { BusinessProfileEditWorkDaysComponent } from '../components/business-profile-edit-work-days/business-profile-edit-work-days.component';
 
 @NgModule({
   declarations: [
     BusinessProfileComponent,
-    BusinessProfileBodyComponent
+    BusinessProfileBodyComponent,
+    BusinessProfileEditPasswordComponent,
+    BusinessProfileEditBankDataComponent,
+    BusinessProfileEditDataComponent,
+    BusinessProfileEditWorkDaysComponent
+
   ],
   imports: [
     CommonModule,
