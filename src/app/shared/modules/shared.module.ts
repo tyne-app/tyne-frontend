84fe55b7e7e74757b68ef79d5402f92f1510b898
David Moya <dav.moyam@outlook.cl>
2021-08-15 22:41:57 -0400

    [fix] fixed material dialog folder route

--- src/app/shared/modules/shared.module.ts
+++ src/app/shared/modules/shared.module.ts
@@ -1,22 +1,18 @@
-
-/**MODULES */
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MaterialModule } from './material.module';
-import { ReactiveFormsModule } from '@angular/forms';
-import  {RouterModule } from '@angular/router';
-
-/**COMPONENTS */
-import { MapComponent } from '../components/map/components/map.component';
-import { CloseModalComponent } from '../components/close-modal/close-modal.component';
-import { SpinnerComponent } from '../components/spinner/spinner.component';
-import { NotFoundPageComponent } from '../components/not-found-page/not-found-page.component';
-
-/**DIRECTIVES */
-import { PhoneMaskDirective } from '../directives/phone-mask.directive';
-import { SanitizeHtmlDirective } from '../directives/sanitize-html.directive';
-import { SafeHtml } from '../pipe/safeHtml.pipe';
-
+/** MODULES */
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { CloseModalComponent } from "../components/close-modal/close-modal.component";
+/** COMPONENTS */
+import { MapComponent } from "../components/map/components/map.component";
+import { NotFoundPageComponent } from "../components/not-found-page/not-found-page.component";
+import { SpinnerComponent } from "../components/spinner/spinner.component";
+/** DIRECTIVES */
+import { PhoneMaskDirective } from "../directives/phone-mask.directive";
+import { SanitizeHtmlDirective } from "../directives/sanitize-html.directive";
+import { SafeHtml } from "../pipe/safeHtml.pipe";
+import { MaterialModule } from "./material.module";
 
 @NgModule({
   declarations: [
@@ -27,14 +23,7 @@ import { SafeHtml } from '../pipe/safeHtml.pipe';
     PhoneMaskDirective,
     SanitizeHtmlDirective,
     SafeHtml,
-    NotFoundPageComponent
-  ],
-  imports: [
-    CommonModule,
-    MaterialModule,
-    ReactiveFormsModule,
-    RouterModule,
- 
+    NotFoundPageComponent,
   ],
   imports: [CommonModule, MaterialModule, ReactiveFormsModule, RouterModule],
   exports: [
@@ -45,7 +34,7 @@ import { SafeHtml } from '../pipe/safeHtml.pipe';
     SpinnerComponent,
     SanitizeHtmlDirective,
     SafeHtml,
-    NotFoundPageComponent
-  ]
+    NotFoundPageComponent,
+  ],
 })
 export class SharedModule {}
