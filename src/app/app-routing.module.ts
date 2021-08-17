5bde696b084d1ef96ca74637b45db2eecd544fd2
David Moya <dav.moyam@outlook.cl>
2021-08-17 17:01:01 -0400

    [fix] business registration route problem

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,67 +1,89 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { NotFoundPageComponent } from './shared/components/not-found-page/not-found-page.component';
-import { TyneRoutes } from './shared/constants/url-routes';
- 
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { NotFoundPageComponent } from "./shared/components/not-found-page/not-found-page.component";
+import { TyneRoutes } from "./shared/constants/url-routes";
+
 /** TODO: ADD GUARD VERY CASE */
 const routes: Routes = [
   {
-    path: TyneRoutes.Init,
-    loadChildren: () => import('./home/modules/home.module').then( m => m.HomeModule),
+    path: TyneRoutes.Home,
+    loadChildren: () =>
+      import("./home/modules/home.module").then((m) => m.HomeModule),
   },
   {
-    path: 'detalle-local',
-    loadChildren: () => import('./business-details/modules/business-details.module').then( m=>m.BusinessDetailsModule),
+    path: "detalle-local",
+    loadChildren: () =>
+      import("./business-details/modules/business-details.module").then(
+        (m) => m.BusinessDetailsModule
+      ),
   },
   {
-    path: TyneRoutes.CommensalRegister,
-    loadChildren: () => import('./business-registration/modules/business-registration.module').then( m => m.BusinessRegistrationModule),
+    path: TyneRoutes.BusinessRegister,
+    loadChildren: () =>
+      import(
+        "./business-registration/modules/business-registration.module"
+      ).then((m) => m.BusinessRegistrationModule),
   },
   {
     path: TyneRoutes.ClientProfile,
-    loadChildren: () => import('./client-profile/modules/client-profile.module').then(m=>m.ClientProfileModule)
+    loadChildren: () =>
+      import("./client-profile/modules/client-profile.module").then(
+        (m) => m.ClientProfileModule
+      ),
   },
   {
-    path: TyneRoutes.LocalProfile,
-    loadChildren: () => import('./business-profile/modules/business-registration.module').then(m=>m.BusinessProfileModule)
+    path: TyneRoutes.BusinessProfile,
+    loadChildren: () =>
+      import("./business-profile/modules/business-registration.module").then(
+        (m) => m.BusinessProfileModule
+      ),
   },
   {
     path: TyneRoutes.Refund,
-    loadChildren: () => import('./refund-policy/modules/refund-policy.module').then( m => m.RefundPolicyModule),
+    loadChildren: () =>
+      import("./refund-policy/modules/refund-policy.module").then(
+        (m) => m.RefundPolicyModule
+      ),
   },
   {
     path: TyneRoutes.FrecuentQuestion,
-    loadChildren: () => import('./frecuent-questions/modules/frequent-questions.module').then( m => m.FrequentQuestionsModule),
+    loadChildren: () =>
+      import("./frecuent-questions/modules/frequent-questions.module").then(
+        (m) => m.FrequentQuestionsModule
+      ),
   },
   {
     path: TyneRoutes.Privacity,
-    loadChildren: () => import('./privacy/modules/privacy.module').then( m=> m.PrivacyModule)
+    loadChildren: () =>
+      import("./privacy/modules/privacy.module").then((m) => m.PrivacyModule),
   },
   {
-    path: 'buscar-locales',
-    loadChildren: () => import('./search-restaurant/modules/search-restaurant.module').then( m=> m.SearchRestaurantModule)
+    path: "buscar-locales",
+    loadChildren: () =>
+      import("./search-restaurant/modules/search-restaurant.module").then(
+        (m) => m.SearchRestaurantModule
+      ),
   },
   {
     path: TyneRoutes.Menu,
-    loadChildren: () => import('./business-menus/modules/business-menus.module').then( m=> m.BusinessMenusModule)
+    loadChildren: () =>
+      import("./business-menus/modules/business-menus.module").then(
+        (m) => m.BusinessMenusModule
+      ),
   },
   {
     path: TyneRoutes.NotFound,
-    component: NotFoundPageComponent
+    component: NotFoundPageComponent,
   },
-  { path: '', 
-    redirectTo: TyneRoutes.Init, 
-    pathMatch: 'full' },
+  { path: "", redirectTo: TyneRoutes.Home, pathMatch: "full" },
   {
-    path: '**',
-    redirectTo: TyneRoutes.NotFound 
+    path: "**",
+    redirectTo: TyneRoutes.NotFound,
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
-
-
+export class AppRoutingModule {}
