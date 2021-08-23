97a031ca2aca0c33665621ed71e6b90302077056
AndromedaK <silverhaloo@gmail.com>
2021-08-22 21:37:43 -0400

    [install] angularFire2 and [Setup firebaseConfig

---
+++ src/app/shared/services/social.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SocialService {
+
+  public constructor() { }
+
+  public facebookLogin(): void{}
+
+  public googleLogin(): void{}
+
+  
+}
