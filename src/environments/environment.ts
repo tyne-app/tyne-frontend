9c0570ac07692fc432e29effb2d3641b1bceddd8
David Moya <dav.moyam@outlook.cl>
2021-08-24 00:06:43 -0400

    [add] added business registration logic

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,10 @@
 export const environment = {
   production: false,
-  mapboxToken: 'pk.eyJ1Ijoib3RoZXJ3b3JsZGx5IiwiYSI6ImNrb3hseGkyNzAyOTYyb3RheXk4djh1cDgifQ.OFfTeR-AmNqQGLvKSj7xrA',
-  API_BASE_CLIENTS:  'https://api-clients-tyne.herokuapp.com/v1',
-  apiBackoffice: 'https://ms-backoffice-tyne.herokuapp.com/',
+  mapboxToken:
+    "pk.eyJ1Ijoib3RoZXJ3b3JsZGx5IiwiYSI6ImNrb3hseGkyNzAyOTYyb3RheXk4djh1cDgifQ.OFfTeR-AmNqQGLvKSj7xrA",
+  API_BASE_CLIENTS: "https://api-clients-tyne.herokuapp.com/v1",
+  apiBackoffice: "https://ms-backoffice-tyne.herokuapp.com/",
+  apiLocals: "https://api-locals-tyne.herokuapp.com/v1",
   firebaseConfig: {
     apiKey: "AIzaSyDym-wsAA7O0Z7RkI32A1Og1s8LaJNa5s0",
     authDomain: "tyne-app.firebaseapp.com",
@@ -10,7 +12,6 @@ export const environment = {
     storageBucket: "tyne-app.appspot.com",
     messagingSenderId: "59995458327",
     appId: "1:59995458327:web:05766e87be7f50315a8552",
-    measurementId: "G-J32CDMHN57"
-  }
+    measurementId: "G-J32CDMHN57",
+  },
 };
-
