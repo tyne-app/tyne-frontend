89bc8dc7bac7a61c24790ae607a1caf6572cf0dc
Matias-15 <mcriado.cg@gmail.com>
2021-08-28 23:01:18 -0400

    Modal dialog business work days

--- src/app/business-profile/components/business-profile-edit-work-days/business-profile-edit-work-days.component.ts
+++ src/app/business-profile/components/business-profile-edit-work-days/business-profile-edit-work-days.component.ts
@@ -11,4 +11,22 @@ export class BusinessProfileEditWorkDaysComponent implements OnInit {
   public constructor() { }
 
   public ngOnInit(): void { }
+
+  public isChecked(value): string {
+    return value === false
+      ? 'Cerrado'
+      : 'Abierto';
+  }
+
+  public weeksValues = [
+    { day: 'Lunes', check: false, openValue: '', closeValue: '' },
+    { day: 'Martes', check: false, openValue: '', closeValue: '' },
+    { day: 'Miércoles', check: true, openValue: '9:00', closeValue: '20:00' },
+    { day: 'Jueves', check: true, openValue: '12:00', closeValue: '23:00' },
+    { day: 'Viernes', check: true, openValue: '11:00', closeValue: '22:00' },
+    { day: 'Sábado', check: true, openValue: '12:00', closeValue: '21:00' },
+    { day: 'Domingo', check: true, openValue: '9:00', closeValue: '20:00' },
+  ]
+
+
 }
