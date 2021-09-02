c117b56843359becc1a9d917bbce6f2ef8a5f336
Matias-15 <mcriado.cg@gmail.com>
2021-09-02 19:29:43 -0400

    Responsive styles client registration component

--- src/app/business-profile/routes/business-registration-routing.module.ts
+++ src/app/business-profile/routes/business-registration-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { BusinessProfileComponent } from '../pages/business-profile.component';
 
 const routes: Routes = [
-   {path: '', component: BusinessProfileComponent}
+    { path: '', component: BusinessProfileComponent }
 ];
 
 @NgModule({
