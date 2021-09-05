48e6ae4d445b831ded2141df201b022b9e1b5a0a
AndromedaK <silverhaloo@gmail.com>
2021-09-05 13:39:20 -0300

    [add] invoke dialog service

---
+++ src/app/shared/helpers/invoke-dialog-component.service.ts
@@ -0,0 +1,24 @@
+/** ANGULAR CORE */
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InvokeDialogComponentService {
+
+  public constructor() { }
+  
+
+  public openLogin(){
+
+  }
+
+  public openSpinner(){
+   
+  }
+  
+  public openRegister(){
+
+  }
+
+}
