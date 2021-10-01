d794bb57a0ff1e46f7baada4f59c006452191e84
David Moya <dav.moyam@outlook.cl>
2021-09-30 22:32:49 -0300

    [update] fix business details

--- src/app/shared/errors/global-error-handler.ts
+++ src/app/shared/errors/global-error-handler.ts
@@ -1,13 +1,11 @@
 import { ErrorHandler, Injectable, NgZone } from "@angular/core";
+import { Router } from "@angular/router";
 import { DialogService } from "../components/components/dialog/services/dialog.service";
 import { errorContent } from "../inmutable/constants/dialog-messages";
 
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
-  public constructor(
-    private zone: NgZone,
-    private dialogService: DialogService
-  ) {}
+  public constructor(private zone: NgZone, private dialogService: DialogService, private router: Router) {}
 
   public handleError(error: Error): void {
     if (!error) return;
