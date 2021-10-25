00961150bcc17cbc0cab9f1a4aa03f2fbcbf4491
Matias-15 <mcriado.cg@gmail.com>
2021-10-25 01:06:44 -0300

    Components carousel-upload, close-upload

---
+++ src/app/business-profile/components/business-profile-close-upload-image/business-profile-close-upload-image.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-business-profile-close-upload-image',
+  templateUrl: './business-profile-close-upload-image.component.html',
+  styleUrls: ['./business-profile-close-upload-image.component.scss']
+})
+export class BusinessProfileCloseUploadImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
