734d33dae98f21ed295709876346bb05a7ac0ecc
AndromedaK <silverhaloo@gmail.com>
2021-11-05 01:21:38 -0300

    [add] image by default

---
+++ src/app/business-menu/modules/business-menu.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BusinessMenuRoutingModule } from '../routes/business-menu-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BusinessMenuRoutingModule
+  ]
+})
+export class BusinessMenuModule { }
