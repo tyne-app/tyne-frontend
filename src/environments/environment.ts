7bcfaad651c7c0110ebbd3418e5a1e30bbacc2e8
AndromedaK <silverhaloo@gmail.com>
2021-11-06 11:21:44 -0300

    [add] alternative apiTyne develop

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: false,
   mapboxToken: "pk.eyJ1Ijoib3RoZXJ3b3JsZGx5IiwiYSI6ImNrb3hseGkyNzAyOTYyb3RheXk4djh1cDgifQ.OFfTeR-AmNqQGLvKSj7xrA",
-  apiTyne: "https://api-locals-tyne.herokuapp.com/v1",
+  apiTyne: "http://127.0.0.1:8001/v1",
+  //apiTyne: "https://api-locals-tyne.herokuapp.com/v1",
   firebaseConfig: {
     apiKey: "AIzaSyDym-wsAA7O0Z7RkI32A1Og1s8LaJNa5s0",
     authDomain: "tyne-app.firebaseapp.com",
