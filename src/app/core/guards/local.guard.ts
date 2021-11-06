2626617b72497b02d70c5cf4242c117cdaa07f0b
AndromedaK <silverhaloo@gmail.com>
2021-11-06 11:09:46 -0300

    [add] local and client guard

---
+++ src/app/core/guards/local.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import {Token} from "@shared/interfaces/token";
+import {UserType} from "@shared/inmutable/enums/user_type.enum";
+import {TokenService} from "@core/helpers/token.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalGuard implements CanActivate {
+  public constructor(
+    private tokenService:TokenService
+  ) {
+  }
+  public canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    let token: Token;
+    token = this.tokenService.getDecodedJwtToken();
+    return token.id_user == UserType.Manager;
+  }
+
+}
