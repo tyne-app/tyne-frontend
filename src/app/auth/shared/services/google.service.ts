52d6dcc70edf0a9712c53a125bf74e91ad306a31
YJLC11 <yerko.95lc@gmail.com>
2022-04-22 17:54:00 -0400

    TYNE-269: - Message error - Fix login social network

--- src/app/auth/shared/services/google.service.ts
+++ src/app/auth/shared/services/google.service.ts
@@ -1,10 +1,5 @@
 import { Injectable } from "@angular/core";
-import {
-  Auth,
-  GoogleAuthProvider,
-  signInWithPopup,
-  UserCredential,
-} from '@angular/fire/auth';
+import { Auth, GoogleAuthProvider, signInWithPopup, UserCredential } from "@angular/fire/auth";
 import { ClientService } from "@app/client/shared/services/client.service";
 import { UserService } from "@app/core/services/user.service";
 import { from, Observable } from "rxjs";
@@ -16,25 +11,24 @@ import { LoginResponse } from "../interfaces/token";
   providedIn: "root",
 })
 export class GoogleService {
-
   public constructor(
     private auth: Auth,
-    private userService:UserService,
-    private clientService:ClientService,
-    private mapperService:MapperService
-    ) {}
+    private userService: UserService,
+    private clientService: ClientService,
+    private mapperService: MapperService
+  ) {}
 
   public googleSignIn(): Observable<LoginResponse> {
     return this.googleSignInWithPopup().pipe(
-      mergeMap( userCredential => {
+      mergeMap((userCredential) => {
         return this.socialLogin(userCredential.user);
       })
     );
   }
 
-  public googleSignUp(): Observable<unknown>{
+  public googleSignUp(): Observable<unknown> {
     return this.googleSignInWithPopup().pipe(
-      mergeMap( userCredential => {
+      mergeMap((userCredential) => {
         return this.socialRegister(userCredential.user);
       })
     );
@@ -44,22 +38,20 @@ export class GoogleService {
     return from(signInWithPopup(this.auth, new GoogleAuthProvider()));
   }
 
-  private socialLogin(user: UserCredential["user"]): Observable<LoginResponse>{
+  private socialLogin(user: UserCredential["user"]): Observable<LoginResponse> {
     return from(user.getIdToken()).pipe(
-      mergeMap( token => {
+      mergeMap((token) => {
+        console.log(token);
         return this.userService.socialLogin(user.email, token);
       })
     );
   }
 
-  private socialRegister(user: UserCredential["user"]): Observable<unknown>{
+  private socialRegister(user: UserCredential["user"]): Observable<unknown> {
     return from(user.getIdToken()).pipe(
-      mergeMap( token => {
-        return this.clientService.registerWithSocialNetworks(
-          this.mapperService.mapUserGoogleClient(user,token)
-        );
+      mergeMap((token) => {
+        return this.clientService.registerWithSocialNetworks(this.mapperService.mapUserGoogleClient(user, token));
       })
     );
   }
-
 }
