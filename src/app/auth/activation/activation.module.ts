fe4d9c9f62f0a48ed42acc521203bdf79f0e77ea
YJLC11 <yerko.95lc@gmail.com>
2022-06-07 01:25:39 -0400

    TYNE-303: - user activation

--- src/app/auth/activation/activation.module.ts
+++ src/app/auth/activation/activation.module.ts
@@ -1,19 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ActivationComponent } from './activation.component';
-import { SharedModule } from '@app/shared/shared.module';
-import { MatCardModule } from '@angular/material/card';
-import { ButtonModule } from '@app/shared/controls/customs/buttons/button/button.module';
-import { ActivationRoutingModule } from './activation-routing.module';
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ActivationComponent } from "./activation.component";
+import { SharedModule } from "@app/shared/shared.module";
+import { MatCardModule } from "@angular/material/card";
+import { ButtonModule } from "@app/shared/controls/customs/buttons/button/button.module";
+import { ActivationRoutingModule } from "./activation-routing.module";
 @NgModule({
-  imports: [
-    CommonModule,
-    ActivationRoutingModule,
-    SharedModule,
-    MatCardModule,
-    ButtonModule
-  ],
-  declarations: [ActivationComponent]
+  declarations: [ActivationComponent],
+  imports: [CommonModule, ActivationRoutingModule, SharedModule, MatCardModule, ButtonModule],
 })
-export class ActivationModule { }
+export class ActivationModule {}
--- src/app/auth/activation/components/activation-success/activation-success.component.ts
+++ src/app/auth/activation/components/activation-success/activation-success.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-activation-success',
-  templateUrl: './activation-success.component.html',
-  styleUrls: ['./activation-success.component.scss']
+  selector: "app-activation-success",
+  templateUrl: "./activation-success.component.html",
+  styleUrls: ["./activation-success.component.scss"],
 })
 export class ActivationSuccessComponent implements OnInit {
+  public constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  public ngOnInit() {}
 }
