328797d7a78d417a8f119c8d7a2492ece47d78a5
rartigues <rartigues2019@inf.udec.cl>
2022-06-30 10:49:50 -0400

    TYNE-357: -wip Falta cambiar en registro surcursales

--- src/app/core/services/territorials.service.ts
+++ src/app/core/services/territorials.service.ts
@@ -22,4 +22,11 @@ export class TerritorialsService {
     const url = this.urlBase + `/territories/cities/${idCity}/states`;
     return this.http.get<State[]>(url);
   }
+  ///cities/{city_id}/states/available
+  public getAvailableStates(idCity: number): Observable<State[]> {
+    const url = this.urlBase + `/territories/cities/${idCity}/states/available`;
+    return this.http.get<State[]>(url);
+  }
+
+
 }
